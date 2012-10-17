f5d5c75618ab3d8e524fbb853d14c96846d8cc08
ReallyFuzzy <reallyfuzzy@example.com>
2012-10-17 18:20:13 +0100

    Fix for Windows PMS and use opportunity to tidy up a few things.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,13 +1,13 @@
 #################################################
-#			      			#
-# ITV Player Plex v9 Plugin   			#
-#			      			#
-# Version: 	0.2				# 
-# Author: 	James Gould			#
-# Contributors: 				#
-# Created: 	08th Sept 2010			#
-# Last Updated: 17th Sept 2010			#
-#						#
+#                                               #
+# ITV Player Plex v9 Plugin                     #
+#                                               #
+# Version: 	0.2                                 # 
+# Author: 	James Gould                         #
+# Contributors:                                 #
+# Created: 	08th Sept 2010                      #
+# Last Updated: 17th Sept 2010                  #
+#                                               #
 #################################################
 
 # PMS plugin framework
@@ -28,47 +28,48 @@ from xml.dom import minidom
 #
 #################################################
 
-VIDEO_PREFIX 		= "/video/itvplayer"
+VIDEO_PREFIX            = "/video/itvplayer"
 
-ITV_URL              	= "http://www.itv.com"
+ITV_URL                 = "http://www.itv.com"
 ITV_SD_PLAYER_URL       = "%s/itvplayer/video/?Filter=%%s" % ITV_URL
 
 ITV_API_URL             = "http://mercury.itv.com/api/xml/Dotcom"
-ITV_API_TOPTEN_URL	= "%s/Episode/TopTen/?callback=doTopTenProgrammes" % ITV_API_URL
+ITV_API_TOPTEN_URL      = "%s/Episode/TopTen/?callback=doTopTenProgrammes" % ITV_API_URL
 ITV_API_MOSTWATCHED_URL = "%s/Episode/MostWatched/?callback=doMostWatched" % ITV_API_URL
-ITV_API_CHANNEL_URL	= "%s/Programme/PerChannel/%%s/?callback=doForChannel" % ITV_API_URL
-ITV_API_AZ_URL		= "%s/Programme/SearchAtoZ/% %s/?callback=doSearchAtoZ" % ITV_API_URL
-ITV_API_PROGRAM_URL	= "%s/Programme/Index/%%s/?callback=doProgramme" % ITV_API_URL
+ITV_API_CHANNEL_URL     = "%s/Programme/PerChannel/%%s/?callback=doForChannel" % ITV_API_URL
+ITV_API_AZ_URL          = "%s/Programme/SearchAtoZ/% %s/?callback=doSearchAtoZ" % ITV_API_URL
+ITV_API_PROGRAM_URL     = "%s/Programme/Index/%%s/?callback=doProgramme" % ITV_API_URL
 ITV_API_ALLPROG_URL     = "%s/Programme/PerChannel/all/999/?callback=doPerChannel" % ITV_API_URL
 
 ITV_API_DATE_URL        = "%s/Episode/ByDate/%%s/?callback=doProgrammesByDate" % ITV_API_URL
 
-ITV_SD_THUMB_URL =""
+ITV_SD_THUMB_URL        =""
 
 ITV_XML_NS              = "http://schemas.itv.com/2009/05/Mercury/Common/Domain"
 ITV_PROGRAMME_XPATH     = "/d:Response/d:Results/d:Groups/d:Group/d:ProgrammeDetails/d:ProgrammeDetail/d:Programme"
 ITV_EPISODE_XPATH       = "/d:Response/d:Results/d:Groups/d:Group/d:ProgrammeDetails/d:ProgrammeDetail/d:Episodes/d:Episode"
 
-NAME 			= L('Title')
-VIDEO_TITLE		= L('VideoTitle')
+NAME                    = L('Title')
+VIDEO_TITLE             = L('VideoTitle')
 
-TOP_TEN_TITLE		= "Top Ten Programmes"
-MOST_WATCHED_TITLE	= "Most Watched Shows"
-TV_CHANNELS_TITLE	= "TV Channels"
+TOP_TEN_TITLE           = "Recommended Programmes"
+MOST_WATCHED_TITLE      = "Most Watched Shows"
+TV_CHANNELS_TITLE       = "TV Channels"
 TV_GENRES_TITLE         = "Genres"
 TV_DATE_TITLE           = "Date"
-A_Z_TITLE		= "A to Z"
-SEARCH_TITLE		= "Search Programmes"
-SEARCH_SUBTITLE		= "Search for your Programme"
-SEARCH_SUMMARY		= "This lets you search for ITV Programmes"
+A_Z_TITLE               = "A to Z"
+SEARCH_TITLE            = "Search Programmes"
+SEARCH_SUBTITLE         = "Search for your Programme"
+SEARCH_SUMMARY          = "This lets you search for ITV Programmes"
 
-ART           		= 'art-default.png'
-ICON          		= 'icon-default.png'
+ART                     = 'art-default.png'
+ICON                    = 'icon-default.png'
 
-ITV1_LOGO		= "http://www.itv.com/204c6617-781b-4ffb-8464-35bfd92c4765/PreviewFile.jpg.ashx?w=2000&q=100"
-ITV2_LOGO		= "http://www.itv.com/4d6ee1af-6863-48dc-94c8-722521507954/PreviewFile.jpg.ashx?w=2000&q=100"
-ITV3_LOGO		= "http://www.itv.com/ab024d09-781e-42d3-9213-1de9dcc08c80/PreviewFile.jpg.ashx?w=2000&q=100"
-ITV4_LOGO		= "http://www.itv.com/7d62b36b-9f86-4f72-ae82-c54fe90e7540/PreviewFile.jpg.ashx?w=2000&q=100"
+ITV1_LOGO               = "http://www.itv.com/itv1/2b721613-b35d-41d8-afae-b9cb17c43f2c/PreviewFile.jpg.ashx?q=90&v=1&w=2000"
+ITV2_LOGO               = "http://www.itv.com/itv2/40a6e9e0-1a41-40e1-809a-f555f232c93c/PreviewFile.jpg.ashx?q=90&v=1&w=2000"
+ITV3_LOGO               = "http://www.itv.com/itv3/1e3d6795-2ea0-4a31-bdf9-e8a407241d0b/PreviewFile.jpg.ashx?q=90&v=1&w=2000"
+ITV4_LOGO               = "http://www.itv.com/itv4/3399e40e-f4e1-4eaf-a0e1-43285aa25790/PreviewFile.jpg.ashx?q=90&v=1&w=2000"
+CITV_LOGO               = "http://www.itv.com/citv/28688633-3c9e-48af-8c43-117db335332a/PreviewFile.jpg.ashx?q=90&v=1&w=2000"
 
 #################################################
 
@@ -91,7 +92,6 @@ def MainMenu():
     dir.Append(Function(DirectoryItem(RenderEpisodeList, title = TOP_TEN_TITLE, thumb=R(ICON)), url = ITV_API_TOPTEN_URL))
     dir.Append(Function(DirectoryItem(RenderEpisodeList, title = MOST_WATCHED_TITLE, thumb=R(ICON)), url = ITV_API_MOSTWATCHED_URL))   
     dir.Append(Function(DirectoryItem(AddTVChannels, title = TV_CHANNELS_TITLE, thumb=R(ICON))))
-    dir.Append(Function(DirectoryItem(AddGenresList, title = TV_GENRES_TITLE, thumb=R(ICON))))
     dir.Append(Function(DirectoryItem(AddDateList, title = TV_DATE_TITLE, thumb=R(ICON))))
     dir.Append(Function(DirectoryItem(AddAToZ, title = A_Z_TITLE, thumb=R(ICON)))) 
     dir.Append(Function(InputDirectoryItem(SearchResults,SEARCH_TITLE,SEARCH_SUBTITLE,summary=SEARCH_SUMMARY, thumb=R(ICON))))
@@ -107,10 +107,11 @@ def SearchResults(sender,query=None):
 
 def AddTVChannels(sender, query = None, url = None, subtitle = None, sort_list = None, thumb_url = ICON, player_url = ITV_SD_PLAYER_URL):
     dir = MediaContainer(title1 = sender.title2, title2 = sender.itemTitle, viewGroup = "Info")
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV1", thumb = ITV1_LOGO), url = ITV_API_ALLPROG_URL))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV2", thumb = ITV2_LOGO), url = ITV_API_ALLPROG_URL))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV3", thumb = ITV3_LOGO), url = ITV_API_ALLPROG_URL))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV4", thumb = ITV4_LOGO), url = ITV_API_ALLPROG_URL))
+    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV1", thumb = ITV1_LOGO), url = ITV_API_CHANNEL_URL % 'ITV1'))
+    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV2", thumb = ITV2_LOGO), url = ITV_API_CHANNEL_URL % 'ITV2'))
+    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV3", thumb = ITV3_LOGO), url = ITV_API_CHANNEL_URL % 'ITV3'))
+    dir.Append(Function(DirectoryItem(RenderProgramList, title = "ITV4", thumb = ITV4_LOGO), url = ITV_API_CHANNEL_URL % 'ITV4'))
+    dir.Append(Function(DirectoryItem(RenderProgramList, title = "CITV", thumb = CITV_LOGO), url = ITV_API_CHANNEL_URL % 'CITV'))
     return dir
 
 #################################################
@@ -127,29 +128,17 @@ def AddDateList(sender, query = None, url = None, subtitle = None, sort_list = N
    
    for number in range (2, 31):
       date = (today+Datetime.Delta(days = -number))
-      dir.Append(Function(DirectoryItem(RenderEpisodeList, title = date.strftime("%a, %d %b %Y"), thumb=''), url = ITV_API_DATE_URL % date.strftime("%Y/%m/%d"), thumb=''))
+      dir.Append(Function(DirectoryItem(RenderEpisodeList, title = date.strftime("%a, %d %b %Y"), thumb=''), url = ITV_API_DATE_URL % date.strftime("%Y/%m/%d")))
    return dir
 
 #################################################
 
-def AddGenresList(sender, query = None, url = None, subtitle = None, sort_list = None, thumb_url = ICON, player_url = ITV_SD_PLAYER_URL):
-    dir = MediaContainer(title1 = sender.title2, title2 = sender.itemTitle, viewGroup = "Info")
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "Entertainment", thumb=''), url = ITV_API_CHANNEL_URL % "ITV1", genres = 'ENTERTAINMENT'))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "LifeStyle", thumb=''), url = ITV_API_CHANNEL_URL % "ITV1", genres = 'LIFESTYLE'))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "Soaps", thumb=''), url = ITV_API_CHANNEL_URL % "ITV1", genres = 'SOAPS'))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "Drama", thumb=''), url = ITV_API_CHANNEL_URL % "ITV1", genres = 'DRAMA'))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "Sports", thumb=''), url = ITV_API_CHANNEL_URL % "ITV1", genres = 'SPORT'))
-    dir.Append(Function(DirectoryItem(RenderProgramList, title = "No Genres", thumb=''), url = ITV_API_CHANNEL_URL % "ITV1", genres = 'NO GENRES'))
-    
-    return dir
-
-#################################################
-
-def RenderProgramList(sender, url = None, genres = None):
+def RenderProgramList(sender, url = None, sort = None):
 
    # this function generates the highlights, most popular and sub-category lists from an RSS feed
  
    dir = MediaContainer(title1 = sender.title2, title2 = sender.itemTitle, viewGroup = "Info")
+   Log(url)
    content = XML.ElementFromURL(url, isHTML=False, errors='ignore', cacheTime=1800)
    programmes = content.xpath(ITV_PROGRAMME_XPATH,namespaces={'d' : ITV_XML_NS})
 
@@ -157,19 +146,18 @@ def RenderProgramList(sender, url = None, genres = None):
     return MessageContainer(
         "No Programmes Found",
         "No Programmes have been found matching your search\nPlease try again."
-    )
-   test = None
+   )
+   
+   progs = []
    for p in programmes:
-      prog = constructProgramme(p)
-      if genres == None:
-         dir.Append(Function(DirectoryItem(RenderEpisodeList, title = prog.title + ' [' + prog.additionalInfo.episodeCount + ' episodes]', subtitle = prog.getFormattedGenres(), thumb = prog.imageUri, summary = prog.summary() ), url = ITV_API_PROGRAM_URL % prog.id))
-         test = 'x'
-      else:
-         if prog.getFormattedGenres() == genres:
-            dir.Append(Function(DirectoryItem(RenderEpisodeList, title = prog.title + ' [' + prog.additionalInfo.episodeCount + ' episodes]', subtitle = prog.getFormattedGenres(), thumb = prog.imageUri, summary = prog.summary() ), url = ITV_API_PROGRAM_URL % prog.id))
-            test = "x"
+      progs.append(constructProgramme(p))
+      
+   progs.sort(key=lambda x: x.title)
+      
+   for prog in progs:
+      dir.Append(Function(DirectoryItem(RenderEpisodeList, title = prog.title + ' [' + prog.additionalInfo.episodeCount + ' episodes]', thumb = prog.imageUri, summary = prog.summary() ), url = ITV_API_PROGRAM_URL % prog.id))
             
-   if test == None:
+   if len(dir) <= 0:
       return MessageContainer(
         "No Programmes Found",
         "No Programmes have been found matching your search\nPlease try again."
